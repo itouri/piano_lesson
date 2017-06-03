84859a3293cd496a98c3770163a7e00e1c93459e
itouri <k-akiyama@axsh.net>
2017-06-03 11:59:11 +0900

    ioの処理を分離

---
+++ io.js
@@ -0,0 +1,71 @@
+// MIDI接続成功時
+function successCallback(m){
+	console.log("asd");
+	midi = m;
+  // 入力MIDIデバイスの記録
+  var it = midi.inputs.values();
+  for(var o = it.next(); !o.done; o = it.next()){
+  	inputs.push(o.value);
+  	$("#input").append("<li>"+ o.value.name +"</li>");
+  }
+
+  // 出力MIDIデバイスの記録
+  var ot = midi.outputs.values();
+  for(var o = ot.next(); !o.done; o = ot.next()){
+  	outputs.push(o.value);
+  	$("#output").append("<li>"+ o.value.name +"</li>");
+  }
+
+  // 入力MIDIデバイスから入力が来たときの処理の登録
+  for(var cnt=0;cnt < inputs.length;cnt++){
+  	inputs[cnt].onmidimessage = onMIDIEvent;
+  }
+}
+
+// MIDI接続失敗時
+function faildCallback(msg){
+	console.log("[Error]:"+msg);
+}
+
+//入力MIDIデバイスから入力が来たときの処理
+function onMIDIEvent(e){
+  var str = "";
+  var vec = "";
+  for(var i=0, out=[]; i<e.data.length; i++) {
+    str = str + e.data[i].toString(16).substr(-2) + " ";
+
+	// f8だったら無視
+	if ( str == "f8 " || str == "fe " ) {
+		return;
+	}
+
+    // 第二要素である 音階を格納
+    if (i == 1 && e.data[2] != null && e.data[2] != 0) {
+    	_hold.push(e.data[1]);
+    }
+
+    // 第三要素が0なら離したってこと
+    if (e.data[2] == 0) {
+    	for (var j = _hold.length - 1; j >= 0; j--) {
+    		if ( _hold[j] == e.data[1]) {
+    			_hold.splice(j,1);
+    		}
+    	}
+    }
+	console.log(_hold);
+	//console.log(str);
+  }
+  $("#data").text(str);
+
+   // 押してる音階を表示
+  str = ""
+  for (var j = _hold.length - 1; j >= 0; j--) {
+  	str = str + vector[ _hold[j] % 12] + " ";
+  }
+  if ( str == "" ) {
+  	str = "--"
+  }
+  $("#vector").text(str);
+  render();
+  check_ans();
+}
